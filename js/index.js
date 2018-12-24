0371271d5b23a3f25a406d508bcecb3657c74650
zhisong.guan <zhisong.guan@ly.com>
2018-12-24 18:49:49 +0800

    feat:add timer

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-function testObject(){
-    var obj = {name:"gzs",age:11,gender:0};
-    for(k in Object.keys){
-        console.log(obj.k);
-    }
+function testObject () {
+  var obj = {name: 'gzs',age: 11,gender: 0}
+  for (k in Object.getOwnPropertyNames(obj)) {
+    console.log(eval('obj.' + k))
+  }
 }
