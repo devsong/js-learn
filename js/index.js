92b820b79056440a85bfd3bf4a27bdc00c7ba884
zhisong.guan <zhisong.guan@ly.com>
2018-12-26 11:12:01 +0800

    feat:add node support

--- js/index.js
+++ js/index.js
@@ -1,6 +1,27 @@
-function testObject () {
-  var obj = {name: 'gzs',age: 11,gender: 0}
-  for (k in Object.getOwnPropertyNames(obj)) {
-    console.log(eval('obj.' + k))
+var obj = {
+  name: 'gzs',
+  age: 11,
+  gender: 0,
+};
+
+function testObjectLoop0 () {
+  for (k in Object.getOwnPropertyNames (obj)) {
+    console.log (k);
+  }
+  console.log (alert.toString ());
+  console.log ('fib:' + fib (6));
+}
+
+function testObjectLoop1 () {
+  for (var k in Object.keys (obj)) {
+    alert (k);
+  }
+}
+
+function fib (n) {
+  if (n === 1 || n === 0) {
+    return n;
+  } else {
+    return fib (n - 2) + fib (n - 1);
   }
 }
