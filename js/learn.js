d6e071e2052f78df50799371fd04be40b3a3e4bc
zhisong.guan <zhisong.guan@ly.com>
2019-02-01 11:32:27 +0800

    feat:modify customconfirm.js

--- js/learn.js
+++ js/learn.js
@@ -1,8 +1,8 @@
 a = 1;
 console.log(window.a);
-var newW = window.open('http://www.baidu.com');
+var newW = window.open('//www.baidu.com');
 setInterval(() => {
-    if (newW.closed) {
+    if (newW && newW.closed) {
         console.log('closed');
     } else {
         console.log('not closed');
